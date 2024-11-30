5f1d59521c71916bd3a740a590d37e539bc9a3e4
kumardevofficial <112534262+kumardevofficial@users.noreply.github.com>
2024-11-30 12:05:04 +0530

    adding axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const [err, seterr] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          "https://practice-backend-server.vercel.app/"
+        );
+        setData(response.data);
+      } catch (err) {
+        seterr(err.message);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="bg-blue-500">Hello world</h1>
+      <div>{data}</div>
     </>
   );
 }
